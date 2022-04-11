21fe38ba662e5e778baa1981ca437a1571bea133
ముత్యాల హర్షిత్ <91818980+MutyalaHarshith@users.noreply.github.com>
2022-04-11 06:19:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,14 +6,14 @@ Token =os.environ.get("BOT_TOKEN",None)
 updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('''Hai {member.full_name} , \n\n`Iam a Simple Welcome Bot. Add me to your group and make me as admin.\n\n Creator : [Harshith](https://t.me/Harshith_Mutyala) ''')
+ updater.message.reply_text('''Hai {member.full_name} , \n\n`Iam a Simple Welcome Bot. Add me to your group and make me as admin.\n\n Creator : @Harshith_Mutyala ''')
 
 def help(updater,context):
- updater.message.reply_text("➠ `Add Me To Group`\n\nMake Admin Me\n\n Creator : [Harshith](https://t.me/Harshith_Mutyala)")\
+ updater.message.reply_text("➠ `Add Me To Group`\n\nMake Admin Me\n\n @Harshith_Mutyala Is creator Of this BoT")\
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hai {member.full_name} , Welcome to ln Support\n\n💖Thank💖You💖For💖Joining💖')
+        update.message.reply_text(f'Hai {member.full_name} , Welcome to ln Support\n\n💖Thank💖You💖For💖Joining💖 @Harshith_Mutyala')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
